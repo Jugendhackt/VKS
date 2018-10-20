0a7c90e40acd5effd35affff6f9ce7f64127c6a2
Chaostheorie <42151227+Chaostheorie@users.noreply.github.com>
2018-10-20 22:56:27 +0200

    ver 0.2
    save don't work

--- test/app.py
+++ test/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import json
 
 app = Flask(__name__)
@@ -12,7 +12,7 @@ def templates(name=None):
 	return render_template('mp.html', name=name)
 
 @app.route("/sek")
-def list():
+def sek():
 	schuelerliste = json.load(open("schuelerliste2.json"))
 	abwesende = []
 	for i in range(len(schuelerliste)):
@@ -20,9 +20,22 @@ def list():
 			abwesende.append(schuelerliste[i])
 
 	return render_template("mp1.html", liste=abwesende)
-@app.route("/list")
+
+@app.route("/lehrer")
 def all():
 	Schuelerliste = json.load(open("schuelerliste2.json"))
 	return render_template("mp1.html", liste=Schuelerliste)
 
-app.run(debug=True, port=80)
+@app.route("/change_state", methods={"POST"})
+def change():
+	schuelerliste = list(json.load(open("schuelerliste2.json")))
+	anwesende = request.form.keys()
+	print(list(anwesende))
+	for i in range(len(schuelerliste)):
+		if schuelerliste[i]["name"] in anwesende:
+			schuelerliste[i]["status"] = "anwesend"
+		else:
+			schuelerliste[i]["status"] = "abwesend"
+	json.dump(schuelerliste, open("schuelerliste2.json", "w"))
+	return jsonify(schuelerliste)
+app.run(debug=True,host="0.0.0.0", port=80)
