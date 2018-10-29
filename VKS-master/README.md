b4449f319dd3fecb1d1a116dd00e98f5ad8a0416
Chaostheorie <42151227+Chaostheorie@users.noreply.github.com>
2018-10-29 19:40:44 +0100

    Kleine Anpassungen
    Es wurden: unnötige Datein entfernt, Eine (Test) Jahrgangsansicht hinzugefügt und einige Dateinamen geändert sowie, dass ein footer in allen Dokumenten hinzugefügt wurde.

---
+++ VKS-master/README.md
@@ -0,0 +1,7 @@
+# Verbesserte Kommunikation in der Schule
+
+- Lehrer tragen Schüler ein, die nicht anwesend sind
+- Das Sekretariat sieht eine Liste abwesnder Schüler
+- Die Informationen werden in einer JSON-Datei gespeichert
+
+erfordert Root Berechtigung
---
+++ VKS-master/VKS/app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, jsonify, request, redirect
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return render_template("schuelerliste.html")
+
+@app.route("/sek")
+def sek():
+	schuelerliste = json.load(open("schuelerliste-9b.json"))
+	abwesende = []
+	for i in range(len(schuelerliste)):
+		if schuelerliste[i]["status"] == "abwesend":
+			abwesende.append(schuelerliste[i])
+
+	return render_template("seki-view.html", liste=abwesende)
+
+@app.route("/lehrer")
+def absence():
+	Schuelerliste = json.load(open("schuelerliste-9b.json"))
+	return render_template("teacher-view.html", liste=Schuelerliste)
+
+@app.route("/change_state", methods={"POST"})
+def change():
+	schuelerliste = list(json.load(open("schuelerliste-9b.json")))
+	anwesende = list(request.form.keys())
+	for i in range(len(schuelerliste)):
+		if schuelerliste[i]["name"] in anwesende:
+			schuelerliste[i]["status"] = "anwesend"
+		else:
+			schuelerliste[i]["status"] = "abwesend"
+	json.dump(schuelerliste, open("schuelerliste-9b.json", "w"))
+	return redirect("/lehrer")
+
+@app.route("/style.css")
+def style():
+	return render_template("style.css")
+
+@app.route("/jahrgaenge-uebersicht")
+def all():
+	return render_template("overview.html")
+@app.route("/overview-9B")
+def test():
+	return render_template("overview-9b.html")
+app.run(debug=True,host="0.0.0.0", port=5000)
