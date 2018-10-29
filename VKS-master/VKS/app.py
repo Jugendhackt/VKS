d3df800956ffbd368789944dc1fe4bd90d2153ca
Chaostheorie <42151227+Chaostheorie@users.noreply.github.com>
2018-10-29 23:39:03 +0100

    Roadmap
    Hinzufügen der Roadmap.

--- VKS-master/VKS/app.py
+++ VKS-master/VKS/app.py
@@ -41,8 +41,14 @@ def style():
 @app.route("/jahrgaenge-uebersicht")
 def all():
 	return render_template("overview.html")
+
 @app.route("/overview-9b")
-def test():
+def overview():
 	all = json.load(open("schuelerliste-9b.json"))
 	return render_template("overview-9b.html", liste=all)
+
+@app.route("/roadmap")
+def roadmap():
+	return render_template("roadmap.html")
+
 app.run(debug=True,host="0.0.0.0", port=5000)
