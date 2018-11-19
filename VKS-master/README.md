dc56cebb7c1cdf4553b4faa06679695f82408033
Chaostheorie <cathero.official@gmail.com>
2018-11-19 20:47:24 +0100

    remove useless files

--- VKS-master/README.md
+++
@@ -1,7 +0,0 @@
-# Verbesserte Kommunikation in der Schule
-
-- Lehrer tragen Schüler ein, die nicht anwesend sind
-- Das Sekretariat sieht eine Liste abwesnder Schüler
-- Die Informationen werden in einer JSON-Datei gespeichert
-
-erfordert Root Berechtigung
--- VKS-master/VKS/gen_terms.py
+++
@@ -1,5 +0,0 @@
-import json
-term1 = {"name":"Tag der offenen Tür", "date":"31. 10. 2018", "author":"Byakushiki", "content":"Tag der offenen Tür mit der Maus."}
-term2 = {"name":"Tag der geschlossenen Tür", "date":"30. 10. 2018", "author":"Byakushiki", "content":"Tag der geschlossenen Tür mit der Maus."}
-term_list = (term1, term2)
-json.dump(term_list, open("terms.json", "w"))
